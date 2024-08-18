7f41bf2e06cf353e3b8acc5098ed27621a27fa92
Shafin Jadavji <shafinj@gmail.com>
2024-08-18 18:09:41 -0400

    modified: created EmotionDetection package
    Signed-off-by: Shafin Jadavji <shafinj@gmail.com>

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-from emotion_detection import emotion_detector
+from EmotionDetection.emotion_detection import emotion_detector
 
 def detector(text_to_analyse):
         response = emotion_detector(text_to_analyse)
